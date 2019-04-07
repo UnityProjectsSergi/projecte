a10dc5d1e3750a95375e994c0ad45d785b9bbf4f
SERGI-PORTATIL\Sergi Regi <sergiregi88@gmail.com>
2019-04-07 21:24:23 +0200

    Add clases slot

--- Projecte3/Assets/Scripts/CuttingSlot.cs
+++ Projecte3/Assets/Scripts/CuttingSlot.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine;
+
 namespace Assets.Scripts
 {
-    public abstract class Table :MonoBehaviour
+    public class CuttingSlot : Slot
     {
-        public bool isActive;
-        public bool hasObjectOn;
-        public bool objectOn;
+
     }
 }
---
+++ Projecte3/Assets/Scripts/Slot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+    public abstract class Slot :MonoBehaviour
+    {
+        public bool isActive;
+        public bool hasObjectOn;
+        public bool objectOn;
+        public void Catch()
+        {
+            
+        }
+        public void LeaveOn()
+        {
+
+        }
+    }
+
