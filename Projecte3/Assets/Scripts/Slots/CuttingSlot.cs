7b1b507e9dae459b30a20fa675a964d376e0eb9f
IzzyCap <47435445+IzzyCap@users.noreply.github.com>
2019-04-15 15:10:12 +0100

    CutSlot and Animation cut

--- Projecte3/Assets/Scripts/Slots/CuttingSlot.cs
+++ Projecte3/Assets/Scripts/Slots/CuttingSlot.cs
@@ -3,23 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
 
+public class CuttingSlot : Slot
+{
+    private float fillAmount = 0;
+    public float timerToAction;
+    private float progresSpeed;
+    public GameObject barCanvas;
+    public Image progresBar;
 
-    public class CuttingSlot : Slot
-    { 
+    private void Start()
+    {
+        progresSpeed = 1 / timerToAction;
+    }
+
     public override void LeaveObjOn(CharacterControllerAct player)
     {
         Item i = player.attachedObject.GetComponent<Item>();
         base.LeaveObjOn(player);
-        if (i.itemType == ItemType.Ing)
-            item.stateIngredient = StateIngredient.cutting;
 
+        if (i.itemType == ItemType.Ing)
+            if (i.stateIngredient == StateIngredient.raw)
+            {
+                barCanvas.SetActive(true);
+                progresBar.fillAmount = 0;
+            }
     }
+
     public override void Catch(CharacterControllerAct player)
-    { 
-      
-      if(item.stateIngredient == StateIngredient.cutted)
-        base.Catch(player);
-        
+    {
+        // The player just can catch items cutted or raw
+        if(item.stateIngredient == StateIngredient.cutted || item.stateIngredient == StateIngredient.raw)
+            base.Catch(player);       
+    }
+
+    public override void Action(CharacterControllerAct player)
+    {
+        if (item != null)
+        {
+            if (item.stateIngredient == StateIngredient.raw 
+             || item.stateIngredient == StateIngredient.cutting)
+            {
+                item.stateIngredient = StateIngredient.cutting;
+                fillAmount += progresSpeed * Time.deltaTime;
+                progresBar.fillAmount = fillAmount;
+
+                if (fillAmount >= 1)
+                {
+                    fillAmount = 0;
+                    barCanvas.SetActive(false);
+                    item.transform.localScale = new Vector3(item.transform.localScale.x, 0.1f, item.transform.localScale.z);
+                    item.stateIngredient = StateIngredient.cutted;
+                }
+            }              
+        }
     }
 }
