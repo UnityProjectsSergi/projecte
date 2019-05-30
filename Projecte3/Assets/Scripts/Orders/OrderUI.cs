9d0b913c277cb6b4296cb27978dfc60fd655c2f5
SERGI-PORTATIL\Sergi Regi <sergiregi88@gmail.com>
2019-05-30 21:08:12 +0200

    Addd somechanges

--- Projecte3/Assets/Scripts/Orders/OrderUI.cs
+++ Projecte3/Assets/Scripts/Orders/OrderUI.cs
@@ -33,7 +33,7 @@ public class OrderUI : MonoBehaviour
         timeout = false;
         totalTime = 0;
         SkackeGameObject = GetComponent<SkackeGameObject>();
-        CountDown = this.StartSafeCoroutine(Countdown());
+         this.StartCoroutine(Countdown());
     }
     public void generateItemsUI()
     {
@@ -131,14 +131,17 @@ public class OrderUI : MonoBehaviour
         {
             if (!timeout)
             {
+                Debug.Log("ssssss");
                 timeOutValue = Mathf.Lerp(1.0f, 0.0f, totalTime / duration);
                 TimeOutColor = Color.Lerp(InitColorBarTimeOut, FinishColorBarTimeOut, totalTime / duration);
                 totalTime += Time.deltaTime;
                 if (timeOutValue <= 0.03f)
                     timeout = true;
+                
             }
             yield return null;
         }
+        
     }
 
 }
