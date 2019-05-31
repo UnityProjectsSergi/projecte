c7f7937e6c37992322abaf1acc2417a91abbcda4
SERGI-PORTATIL\Sergi Regi <sergiregi88@gmail.com>
2019-05-31 23:07:51 +0200

    changes

---
+++ Projecte3/Assets/provaaudio.cs
@@ -0,0 +1,24 @@
+﻿using FMOD.Studio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class provaaudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private EventInstance eventInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventInstance = SoundManager.Instance.CreateEventInstaceAttached("event:/Sounds/Cook/Pot/PotIdle", this.gameObject);
+        eventInstance.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //if (Input.GetKeyDown(KeyCode.A))
+          
+    }
+}
