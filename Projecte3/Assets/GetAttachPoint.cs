1b9428a71bc3903dda0e64b8b26fab7e2af03bf0
IzzyCap <47435445+IzzyCap@users.noreply.github.com>
2019-06-13 17:32:43 +0100

    Objectes es mouen amb el player

---
+++ Projecte3/Assets/GetAttachPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAttachPoint : MonoBehaviour
+{
+    public Transform parent;
+    void Start()
+    {
+        this.transform.parent.gameObject.GetComponent<CharacterControllerAct>().attachTransform.parent = parent;
+    }
+}
